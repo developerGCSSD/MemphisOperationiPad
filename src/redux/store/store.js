955ca106cfc080a42d716d0ea6275c03cccc24f5
Fady Nashat <fidoo.nashat@gmail.com>
2025-07-14 12:07:23 +0300

    leaders and Guides assignment

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -3,12 +3,13 @@ import authReducer from '../reducers/auth';
 import assignmentReducer from '../reducers/assignmentLists';
 import departmentsReducer from '../reducers/departmentsList';
 import unassignedfilesReducer from '../reducers/unassignedFiles';
-
+import bulkAssignmentReducer from '../reducers/bulkAssignment';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     assignments: assignmentReducer,
     departments: departmentsReducer,
     unassignedFiles: unassignedfilesReducer,
+    bulkAssignment: bulkAssignmentReducer,
   },
 });
